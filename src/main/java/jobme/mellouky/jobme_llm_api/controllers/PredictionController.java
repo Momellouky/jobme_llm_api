cf2346bd9b110bb8f0e6dd840b85323ef5b07c6a
moMellouky <contact.mellouky@gmail.com>
2024-11-01 11:07:28 +0100

    fix: return json data

--- src/main/java/jobme/mellouky/jobme_llm_api/controllers/PredictionController.java
+++ src/main/java/jobme/mellouky/jobme_llm_api/controllers/PredictionController.java
@@ -2,9 +2,12 @@ package jobme.mellouky.jobme_llm_api.controllers;
 
 import jobme.mellouky.jobme_llm_api.services.IPredictionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/prediction")
@@ -15,11 +18,11 @@ public class PredictionController implements IPredictionController{
     IPredictionService predictionService;
 
     @PostMapping("jobs")
-    public ResponseEntity<String> predict(
+    public ResponseEntity<Map<String, Double>> predict(
             @RequestBody(required = true) String userSkills
     ) {
-       String response = predictionService.predict(userSkills);
-       return new ResponseEntity<String>(response, HttpStatus.OK);
+       Map<String, Double> response = predictionService.predict(userSkills);
+       return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
